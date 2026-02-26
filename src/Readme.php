96a4aeccf2a65845b5ad2df689b6f140b2ca42c6
Pierre Arnissolle <pierre@arnissolle.com>
2026-02-26 22:12:23 +0100

    Enhance Readme with emoji and personal details

--- src/Readme.php
+++ src/Readme.php
@@ -17,10 +17,12 @@ final class Readme
         return <<<'MARKDOWN'
             Ahoy, World! 👋
 
-            I'm Pierre Arnissolle, Backend Engineer at Yousign (soon-to-be Youtrust).
+            👨🏻‍🌾 I'm Pierre Arnissolle, Backend Engineer at Yousign (soon-to-be Youtrust).
 
             🪴 I plan to update [my website]({$self->website}) one day...
+            
             🌐 After which, it will become my main/preffered online presence and will be ActivityPub-compliant.
+            
             💼 In the meantime, I'm only maintaining [my LinkedIn profile]({$self->linkedin}).
             MARKDOWN;
     }
